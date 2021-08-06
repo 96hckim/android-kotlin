453238dd0f852cb4a29316b6f929d1bb4de31b80
96hckim <yp050431@gmail.com>
2021-08-06 14:24:17 +0900

    심리테스트

--- projects/LoveTest/app/src/main/java/com/hocheol/lovetest/MainActivity.kt
+++ projects/LoveTest/app/src/main/java/com/hocheol/lovetest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.hocheol.lovetest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
