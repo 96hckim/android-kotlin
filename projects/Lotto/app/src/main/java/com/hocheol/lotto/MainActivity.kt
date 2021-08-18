86c44497a6cc4bde57c4878db67da85f018cd648
96hckim <yp050431@gmail.com>
2021-08-18 13:21:51 +0900

    [Add] UI

--- projects/Lotto/app/src/main/java/com/hocheol/lotto/MainActivity.kt
+++ projects/Lotto/app/src/main/java/com/hocheol/lotto/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.hocheol.lotto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val btnClear: Button by lazy {
+        findViewById(R.id.btn_clear)
+    }
+
+    private val btnAdd: Button by lazy {
+        findViewById(R.id.btn_add)
+    }
+
+    private val btnRun: Button by lazy {
+        findViewById(R.id.btn_run)
+    }
+
+    private val numberPicker: NumberPicker by lazy {
+        findViewById(R.id.picker)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        initRunButton()
     }
+
+    private fun initRunButton() {
+
+    }
+
 }
