59c4a8c06c588e7da1ef73e57774a84657657b56
96hckim <yp050431@gmail.com>
2021-08-23 16:37:49 +0900

    계산기 기능 구현 (2/2) - Clear, Result 버튼 클릭 이벤트

--- projects/Calculator/app/src/main/java/com/hocheol/calculator/MainActivity.kt
+++ projects/Calculator/app/src/main/java/com/hocheol/calculator/MainActivity.kt
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         tvExpression.append(number)
-        // TODO tvReuslt 실시간으로 계산 결과를 넣어야 하는 기능
+        tvResult.text = calculateExpression()
 
     }
 
@@ -105,7 +105,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clearButtonClicked(v: View) {
-
+        tvExpression.text = ""
+        tvResult.text = ""
+        isOperator = false
+        hasOperator = false
     }
 
     fun historyButtonClicked(v: View) {
@@ -113,7 +116,63 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun resultButtonClicked(v: View) {
+        val expressionTexts = tvExpression.text.split(" ")
+
+        if (tvExpression.text.isEmpty() || expressionTexts.size == 1) {
+            return
+        }
+
+        if (expressionTexts.size != 3 && hasOperator) {
+            Toast.makeText(this, "아직 완성되지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            Toast.makeText(this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val expressionText = tvExpression.text.toString()
+        val resultText = calculateExpression()
+
+        tvResult.text = ""
+        tvExpression.text = resultText
+
+        isOperator = false
+        hasOperator = false
 
     }
 
+    private fun calculateExpression(): String {
+        val expressionTexts = tvExpression.text.split(" ")
+
+        if (hasOperator.not() || expressionTexts.size != 3) {
+            return ""
+        } else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            return ""
+        }
+
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
+
+        return when (op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "*" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
+    }
+
+}
+
+fun String.isNumber(): Boolean {
+    return try {
+        this.toBigInteger()
+        true
+    } catch (e: NumberFormatException) {
+        false
+    }
 }
