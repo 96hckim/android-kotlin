11885cea673b39ae1d9b7d99221475997f2a92df
96hckim <yp050431@gmail.com>
2021-08-30 15:30:23 +0900

    효과음 추가

--- projects/PomodoroTimer/app/src/main/java/com/hocheol/pomodorotimer/MainActivity.kt
+++ projects/PomodoroTimer/app/src/main/java/com/hocheol/pomodorotimer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hocheol.pomodorotimer
 
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.SeekBar
@@ -20,19 +21,43 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
     private var currentCountDownTimer: CountDownTimer? = null
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
     }
 
     private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
                     if (fromUser) {
                         updateRemainTime(progress * 60 * 1000L)
                     }
@@ -46,12 +71,16 @@ class MainActivity : AppCompatActivity() {
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return
 
-                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
-                    currentCountDownTimer?.start()
+                    startCountDown()
                 }
 
             }
         )
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 
     private fun createCountDownTimer(initialMillis: Long): CountDownTimer =
@@ -62,10 +91,28 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                updateRemainTime(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
         }
+
+    private fun startCountDown() {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let {
+            soundPool.play(it, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let {
+            soundPool.play(it, 1F, 1F, 0, 0, 1F)
+        }
+    }
 
     private fun updateRemainTime(remainMillis: Long) {
         val remainSeconds = remainMillis / 1000
