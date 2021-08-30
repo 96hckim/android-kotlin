269f8b9a1735e98940a47b1da6ab3e32137ca5ef
96hckim <yp050431@gmail.com>
2021-08-30 14:25:18 +0900

    기본 UI 구성

--- projects/PomodoroTimer/app/src/main/java/com/hocheol/pomodorotimer/MainActivity.kt
+++ projects/PomodoroTimer/app/src/main/java/com/hocheol/pomodorotimer/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.hocheol.pomodorotimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
     }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    remainMinutesTextView.text = "%02d".format(p1)
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+
+            }
+        )
+    }
+
 }
