0683a8f1dcd0225559b15f942063b9918c0add29
96hckim <yp050431@gmail.com>
2021-09-01 18:52:00 +0900

    URL 로딩 기능 구현

--- projects/SimpleWebBrowser/app/src/main/java/com/hocheol/simplewebbrowser/MainActivity.kt
+++ projects/SimpleWebBrowser/app/src/main/java/com/hocheol/simplewebbrowser/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.hocheol.simplewebbrowser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
     }
+
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(textView.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
+    }
+
 }
