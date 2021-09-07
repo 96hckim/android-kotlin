2436a5b32b4cf476711abd5dd8d5e55fbc0e5122
96hckim <yp050431@gmail.com>
2021-09-07 14:09:29 +0900

    Cloud Messaging 구성

--- projects/PushNotificationReceiver/app/src/main/java/com/hocheol/pushnotificationreceiver/MainActivity.kt
+++ projects/PushNotificationReceiver/app/src/main/java/com/hocheol/pushnotificationreceiver/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hocheol.pushnotificationreceiver
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +18,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                firebaseToken.text = task.result
+            }
+        }
     }
 
 }
