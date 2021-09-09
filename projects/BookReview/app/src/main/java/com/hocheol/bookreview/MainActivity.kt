2ac00d4eb97361d7f99e7992cc329e704b8e9c29
96hckim <yp050431@gmail.com>
2021-09-10 08:44:22 +0900

    인터파크 도서 Open API 통해 도서 목록 가져오기

--- projects/BookReview/app/src/main/java/com/hocheol/bookreview/MainActivity.kt
+++ projects/BookReview/app/src/main/java/com/hocheol/bookreview/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.hocheol.bookreview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.hocheol.bookreview.api.BookService
+import com.hocheol.bookreview.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks(API_KEY)
+            .enqueue(object : Callback<BestSellerDto> {
+
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT!! SUCCESS", )
+                        return
+                    }
+
+                    response.body()?.let {
+                        Log.d(TAG, it.toString())
+
+                        it.books.forEach { book ->
+                            Log.d(TAG, book.toString())
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                }
+
+            })
     }
+
+    companion object {
+        private const val API_KEY = "5A72EC06545F22DD9F567A545C3C91A3C103F65F8E0E3353E16759C6B2CA35C9"
+        private const val TAG = "MainActivity"
+    }
+
 }
---
+++ projects/BookReview/app/src/main/java/com/hocheol/bookreview/api/BookService.kt
@@ -0,0 +1,22 @@
+package com.hocheol.bookreview.api
+
+import com.hocheol.bookreview.model.BestSellerDto
+import com.hocheol.bookreview.model.SearchBookDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BookService {
+
+    @GET("/api/search.api?output=json")
+    fun getBooksByName(
+        @Query("key") apiKey: String,
+        @Query("query") keyword: String
+    ): Call<SearchBookDto>
+
+    @GET("/api/bestSeller.api?output=json&categoryId=100")
+    fun getBestSellerBooks(
+        @Query("key") apiKey: String
+    ): Call<BestSellerDto>
+
+}
