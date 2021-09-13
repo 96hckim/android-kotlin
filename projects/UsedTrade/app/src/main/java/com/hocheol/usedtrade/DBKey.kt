20a39c099f691b5f6460c4b6feeabc2d47039952
96hckim <yp050431@gmail.com>
2021-09-13 11:41:30 +0900

    Firebase Setup And Article List Get From Realtime Database

---
+++ projects/UsedTrade/app/src/main/java/com/hocheol/usedtrade/DBKey.kt
@@ -0,0 +1,7 @@
+package com.hocheol.usedtrade
+
+class DBKey {
+    companion object {
+        const val DB_ARTICLES = "Articles"
+    }
+}
--- projects/UsedTrade/app/src/main/java/com/hocheol/usedtrade/home/ArticleModel.kt
+++ projects/UsedTrade/app/src/main/java/com/hocheol/usedtrade/home/ArticleModel.kt
@@ -1,9 +1,13 @@
 package com.hocheol.usedtrade.home
 
 data class ArticleModel(
-    val sellerId : String,
+    val sellerId: String,
     val title: String,
     val createAt: Long,
     val price: String,
     val imageUrl: String
-)
+) {
+
+    constructor() : this("", "", 0, "", "")
+
+}
