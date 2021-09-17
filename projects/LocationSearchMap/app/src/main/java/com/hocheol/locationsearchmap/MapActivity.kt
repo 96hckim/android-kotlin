43962b74d62a3d185f3220c9b4cbca09ff3ffa3c
96hckim <yp050431@gmail.com>
2021-09-17 13:34:47 +0900

    내 위치 가져오기

--- projects/LocationSearchMap/app/src/main/java/com/hocheol/locationsearchmap/MapActivity.kt
+++ projects/LocationSearchMap/app/src/main/java/com/hocheol/locationsearchmap/MapActivity.kt
@@ -1,7 +1,16 @@
 package com.hocheol.locationsearchmap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,6 +19,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.hocheol.locationsearchmap.databinding.ActivityMapBinding
+import com.hocheol.locationsearchmap.model.LocationLatLngEntity
 import com.hocheol.locationsearchmap.model.SearchResultEntity
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -20,9 +30,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var searchResult: SearchResultEntity
 
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var currentLocationListener: CurrentLocationListener
+
     companion object {
         const val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
         const val CAMERA_ZOOM_LEVEL = 17f
+        const val PERMISSION_REQUEST_CODE = 101
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +50,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 searchResult = it.getParcelableExtra(SEARCH_RESULT_EXTRA_KEY) ?: throw Exception("데이터가 존재하지 않습니다.")
                 setupGoogleMap()
             }
+        }
+
+        bindViews()
+    }
+
+    private fun bindViews() = with(binding) {
+        currentLocationButton.setOnClickListener {
+            getCurrentLocation()
         }
     }
 
@@ -64,6 +87,82 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, CAMERA_ZOOM_LEVEL))
 
         return map.addMarker(markerOptions)
+    }
+
+    private fun getCurrentLocation() {
+        if (::locationManager.isInitialized.not()) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (isGpsEnabled) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                setCurrentLocationListener()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setCurrentLocationListener() {
+        val minTime = 1500L
+        val minDistance = 100f
+
+        if (::currentLocationListener.isInitialized.not()) {
+            currentLocationListener = CurrentLocationListener()
+        }
+        with(locationManager) {
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                currentLocationListener
+            )
+            requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                minTime,
+                minDistance,
+                currentLocationListener
+            )
+        }
+    }
+
+    private fun onCurrentLocationChanged(locationLatLngEntity: LocationLatLngEntity) {
+        map.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    locationLatLngEntity.latitude.toDouble(),
+                    locationLatLngEntity.longitude.toDouble()
+                ), CAMERA_ZOOM_LEVEL
+            )
+        )
+    }
+
+    inner class CurrentLocationListener : LocationListener {
+
+        override fun onLocationChanged(location: Location) {
+            val locationLatLngEntity = LocationLatLngEntity(
+                location.latitude.toFloat(),
+                location.longitude.toFloat()
+            )
+            onCurrentLocationChanged(locationLatLngEntity)
+        }
+
     }
 
 }
