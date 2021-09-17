36ab532c9a2e0d5b5c5151dfee1d83acd3c5c10d
96hckim <yp050431@gmail.com>
2021-09-17 11:14:55 +0900

    API 서비스 및 빌더, Model 클래스 생성

---
+++ projects/LocationSearchMap/app/src/main/java/com/hocheol/locationsearchmap/Key.kt
@@ -0,0 +1,5 @@
+package com.hocheol.locationsearchmap
+
+object Key {
+    const val TMAP_API = "l7xx00d89c7d9f31472fab2b8c6036eabee7"
+}
---
+++ projects/LocationSearchMap/app/src/main/java/com/hocheol/locationsearchmap/response/search/SearchPoiInfo.kt
@@ -0,0 +1,8 @@
+package com.hocheol.locationsearchmap.response.search
+
+data class SearchPoiInfo(
+    val totalCount: String,
+    val count: String,
+    val page: String,
+    val pois: Pois
+)
