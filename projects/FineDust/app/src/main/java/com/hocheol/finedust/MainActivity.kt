7a85b1c6b463703e22ceab9007abcf228d68bc7e
96hckim <yp050431@gmail.com>
2021-09-22 14:08:07 +0900

    사용자 위치 정보 불러오기

--- projects/FineDust/app/src/main/java/com/hocheol/finedust/MainActivity.kt
+++ projects/FineDust/app/src/main/java/com/hocheol/finedust/MainActivity.kt
@@ -1,11 +1,99 @@
 package com.hocheol.finedust
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.hocheol.finedust.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initVariables()
+        checkLocationPermission()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        cancellationTokenSource?.cancel()
+    }
+
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+        } else {
+            getCurrentLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == LOCATION_PERMISSIONS_REQUEST_CODE &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (locationPermissionGranted) {
+            getCurrentLocation()
+        } else {
+            finish()
+        }
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            LOCATION_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        cancellationTokenSource = CancellationTokenSource()
+
+        fusedLocationProviderClient
+            .getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener { location ->
+                binding.textView.text = "${location.latitude}, ${location.longitude}"
+            }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSIONS_REQUEST_CODE = 100
+    }
+
 }
