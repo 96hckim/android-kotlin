bb99ace8d39ae1c07f67c273a43b85f0f809b790
96hckim <yp050431@gmail.com>
2021-09-23 14:15:56 +0900

    UI 와 데이터 연결하여 화면에 보여주기

--- projects/Unsplash/app/src/main/java/com/hocheol/unsplash/MainActivity.kt
+++ projects/Unsplash/app/src/main/java/com/hocheol/unsplash/MainActivity.kt
@@ -2,19 +2,24 @@ package com.hocheol.unsplash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hocheol.unsplash.data.Repository
+import com.hocheol.unsplash.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val scope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        initViews()
         fetchRandomPhotos()
     }
 
@@ -24,9 +29,17 @@ class MainActivity : AppCompatActivity() {
         scope.cancel()
     }
 
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.adapter = PhotoAdapter()
+    }
+
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
         Repository.getRandomPhotos(query)?.let { photos ->
-            photos
+            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                this.photos = photos
+                notifyDataSetChanged()
+            }
         }
     }
 
