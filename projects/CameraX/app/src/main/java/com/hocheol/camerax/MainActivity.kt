234d928224e9d1ad2274bcbff1caed1c766894e4
96hckim <yp050431@gmail.com>
2021-10-01 05:56:08 +0900

    Camera Preview 구현

--- projects/CameraX/app/src/main/java/com/hocheol/camerax/MainActivity.kt
+++ projects/CameraX/app/src/main/java/com/hocheol/camerax/MainActivity.kt
@@ -1,11 +1,134 @@
 package com.hocheol.camerax
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.display.DisplayManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.hocheol.camerax.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var cameraExecutor: ExecutorService
+    private val cameraMainExecutor by lazy { ContextCompat.getMainExecutor(this) }
+
+    // 카메라 얻어오면 이후 실행 리스너 등록
+    private val cameraProviderFeature by lazy { ProcessCameraProvider.getInstance(this) }
+
+    private lateinit var imageCapture: ImageCapture
+
+    private val displayManager by lazy {
+        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
+    private var displayId: Int = -1
+
+    private var camera:Camera? = null
+
+    private val displayListener = object : DisplayManager.DisplayListener {
+
+        override fun onDisplayAdded(displayId: Int) = Unit
+
+        override fun onDisplayRemoved(displayId: Int) = Unit
+
+        override fun onDisplayChanged(displayId: Int) {
+            if (this@MainActivity.displayId == displayId) {
+
+            }
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera(binding.viewFinder)
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
     }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera(viewFinder: PreviewView) {
+        displayManager.registerDisplayListener(displayListener, null)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        viewFinder.postDelayed({
+            displayId = viewFinder.display.displayId
+            bindCameraUseCase()
+        }, 10)
+    }
+
+    private fun bindCameraUseCase() = with(binding) {
+        val rotation = viewFinder.display.rotation
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(LENS_FACING).build() // 카메라 설정(후면)
+
+        cameraProviderFeature.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFeature.get()
+            val preview = Preview.Builder().apply {
+                setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                setTargetRotation(rotation)
+            }.build()
+
+            val imageCaptureBuilder = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(rotation)
+                .setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+
+            imageCapture = imageCaptureBuilder.build()
+
+            try {
+                cameraProvider.unbindAll() // 기존에 바인딩 되어 있는 카메라는 해제를 해준다.
+                camera = cameraProvider.bindToLifecycle(
+                    this@MainActivity, cameraSelector, preview, imageCapture
+                )
+                preview.setSurfaceProvider(viewFinder.surfaceProvider)
+            } catch (e:Exception) {
+                e.printStackTrace()
+            }
+        }, cameraMainExecutor)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS && allPermissionsGranted()) {
+            startCamera(binding.viewFinder)
+        } else {
+            Toast.makeText(this, "카메라 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private val LENS_FACING: Int = CameraSelector.LENS_FACING_BACK
+    }
+
 }
