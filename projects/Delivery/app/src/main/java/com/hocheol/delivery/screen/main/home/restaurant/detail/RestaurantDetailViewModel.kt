b9679b4caf7dae1693c7f84620fa3eff5e36c333
96hckim <yp050431@gmail.com>
2021-12-17 16:02:59 +0900

    식당 상세화면 전화 기능 구현

--- projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/home/restaurant/detail/RestaurantDetailViewModel.kt
+++ projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/home/restaurant/detail/RestaurantDetailViewModel.kt
@@ -19,4 +19,13 @@ class RestaurantDetailViewModel(
         )
     }
 
+    fun getRestaurantTelNumber(): String? {
+        return when (val data = restaurantDetailStateLiveData.value) {
+            is RestaurantDetailState.Success -> {
+                data.restaurantEntity.restaurantTelNumber
+            }
+            else -> null
+        }
+    }
+
 }
