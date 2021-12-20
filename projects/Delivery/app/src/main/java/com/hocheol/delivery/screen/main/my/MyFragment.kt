367aaafd8e7e4a49d1edf82543ce7e04ff802fb4
96hckim <yp050431@gmail.com>
2021-12-20 16:05:53 +0900

    구글 로그인 연동 구현 - 1

--- projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/my/MyFragment.kt
+++ projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/my/MyFragment.kt
@@ -1,8 +1,14 @@
 package com.hocheol.delivery.screen.main.my
 
+import android.app.Activity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.hocheol.delivery.R
 import com.hocheol.delivery.databinding.FragmentMyBinding
 import com.hocheol.delivery.screen.base.BaseFragment
-import com.hocheol.delivery.screen.main.home.HomeFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyFragment : BaseFragment<MyViewModel, FragmentMyBinding>() {
@@ -10,6 +16,41 @@ class MyFragment : BaseFragment<MyViewModel, FragmentMyBinding>() {
     override val viewModel by viewModel<MyViewModel>()
 
     override fun getViewBinding(): FragmentMyBinding = FragmentMyBinding.inflate(layoutInflater)
+
+    private val gso: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+
+    private val gsc by lazy { GoogleSignIn.getClient(requireContext(), gso) }
+
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    private val loginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                task.getResult(ApiException::class.java)?.let { account ->
+                    viewModel.saveToken(account.idToken ?: throw Exception())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = gsc.signInIntent
+        loginLauncher.launch(signInIntent)
+    }
 
     override fun observeData() {
     }
--- projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/my/MyViewModel.kt
+++ projects/Delivery/app/src/main/java/com/hocheol/delivery/screen/main/my/MyViewModel.kt
@@ -2,5 +2,12 @@ package com.hocheol.delivery.screen.main.my
 
 import com.hocheol.delivery.screen.base.BaseViewModel
 
-class MyViewModel : BaseViewModel() {
+class MyViewModel(
+
+) : BaseViewModel() {
+
+    fun saveToken(idToken: String) {
+
+    }
+
 }
