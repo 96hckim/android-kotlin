840dd256277b9651d7e609e6dcfa357f89ddd430
96hckim <yp050431@gmail.com>
2023-01-29 14:34:31 +0900

    단위 변환 기능 구현

--- projects-2023/part1/02-UnitConverter/app/src/main/java/com/hocheol/unitconverter/MainActivity.kt
+++ projects-2023/part1/02-UnitConverter/app/src/main/java/com/hocheol/unitconverter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hocheol.unitconverter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.hocheol.unitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,34 @@ class MainActivity : AppCompatActivity() {
 
         val inputEditText = binding.inputEditText
         val outputTextView = binding.outputTextView
+        val inputUnitTextView = binding.inputUnitTextView
+        val outputUnitTextView = binding.outputUnitTextView
         val swapImageButton = binding.swapImageButton
+
+        var inputNumber = 0
+        var cmToM = true
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = text?.toString()?.toIntOrNull() ?: 0
+
+            if (cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if (cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }
