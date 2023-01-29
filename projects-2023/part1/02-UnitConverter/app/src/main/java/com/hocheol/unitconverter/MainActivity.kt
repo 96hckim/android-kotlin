2c2d209c3958083b75f96930488b49fc235a2613
96hckim <yp050431@gmail.com>
2023-01-29 14:17:12 +0900

    UI 요소 가져오기 (viewBinding 적용)

--- projects-2023/part1/02-UnitConverter/app/src/main/java/com/hocheol/unitconverter/MainActivity.kt
+++ projects-2023/part1/02-UnitConverter/app/src/main/java/com/hocheol/unitconverter/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.hocheol.unitconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hocheol.unitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val inputEditText = binding.inputEditText
+        val outputTextView = binding.outputTextView
+        val swapImageButton = binding.swapImageButton
     }
 }
