63e4028cec0f33127f5e796f7d2e87ddd0fc3cd5
96hckim <yp050431@gmail.com>
2023-03-25 14:42:05 +0900

    데이터 저장하고 불러오기

--- projects-2023/part1/03-MedicalInformation/app/src/main/java/com/hocheol/medicalinformation/EditActivity.kt
+++ projects-2023/part1/03-MedicalInformation/app/src/main/java/com/hocheol/medicalinformation/EditActivity.kt
@@ -2,8 +2,10 @@ package com.hocheol.medicalinformation
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.hocheol.medicalinformation.databinding.ActivityEditBinding
@@ -46,5 +48,34 @@ class EditActivity : AppCompatActivity() {
             binding.warningValueEditText.isVisible = isChecked
         }
         binding.warningValueEditText.isVisible = binding.warningCheckBox.isChecked
+
+        // 저장
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish()
+        }
+    }
+
+    private fun saveData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameValueEditText.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactValueEditText.text.toString())
+            putString(BIRTHDATE, binding.birthValueTextView.text.toString())
+            putString(WARNING, getWarning())
+            apply()
+        }
+
+        Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningValueEditText.text.toString() else ""
     }
 }
--- projects-2023/part1/03-MedicalInformation/app/src/main/java/com/hocheol/medicalinformation/MainActivity.kt
+++ projects-2023/part1/03-MedicalInformation/app/src/main/java/com/hocheol/medicalinformation/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hocheol.medicalinformation
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.hocheol.medicalinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,28 @@ class MainActivity : AppCompatActivity() {
         binding.editButton.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getDataUiUpdate()
+    }
+
+    private fun getDataUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+
+            val warning = getString(WARNING, "")
+            val isWarningVisible = warning.isNullOrEmpty().not()
+            binding.warningTitleTextView.isVisible = isWarningVisible
+            binding.warningValueTextView.isVisible = isWarningVisible
+            if (isWarningVisible) {
+                binding.warningValueTextView.text = warning
+            }
         }
     }
 }
