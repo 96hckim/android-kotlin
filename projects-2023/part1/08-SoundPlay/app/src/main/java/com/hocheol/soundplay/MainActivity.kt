986631fed6701c526b4014cac1470a1319b5a681
96hckim <yp050431@gmail.com>
2023-04-16 13:09:58 +0900

    MediaPlayer 로 음원재생 (1)

--- projects-2023/part1/08-SoundPlay/app/src/main/java/com/hocheol/soundplay/MainActivity.kt
+++ projects-2023/part1/08-SoundPlay/app/src/main/java/com/hocheol/soundplay/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.hocheol.soundplay
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hocheol.soundplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener {
+            mediaPlayerPlay()
+        }
+
+        binding.pauseButton.setOnClickListener {
+            mediaPlayerPause()
+        }
+
+        binding.stopButton.setOnClickListener {
+            mediaPlayerStop()
+        }
+    }
+
+    private fun mediaPlayerPlay() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.over_the_horizon).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
