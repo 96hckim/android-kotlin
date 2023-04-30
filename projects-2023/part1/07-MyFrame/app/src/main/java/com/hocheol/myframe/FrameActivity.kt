204ab04f94f82d001773108beb83358175290a22
96hckim <yp050431@gmail.com>
2023-04-30 14:17:23 +0900

    액자 UI 그리기 (4)

--- projects-2023/part1/07-MyFrame/app/src/main/java/com/hocheol/myframe/FrameActivity.kt
+++ projects-2023/part1/07-MyFrame/app/src/main/java/com/hocheol/myframe/FrameActivity.kt
@@ -2,6 +2,7 @@ package com.hocheol.myframe
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hocheol.myframe.databinding.ActivityFrameBinding
@@ -13,6 +14,12 @@ class FrameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.apply {
+            title = "나만의 앨범"
+            setSupportActionBar(this)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val images = (intent.getStringArrayExtra("images") ?: emptyArray()).map { uriString ->
             FrameItem(Uri.parse(uriString))
@@ -27,5 +34,16 @@ class FrameActivity : AppCompatActivity() {
         ) { tab, position ->
             binding.imageViewPager.currentItem = tab.position
         }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
