a4951f308c53f97d184f465fa9d78ea1d33631e4
96hckim <yp050431@gmail.com>
2023-05-04 00:19:49 +0900

    WebView 사용해보기

--- projects-2023/part2/01-Webtoon/app/src/main/java/com/hocheol/webtoon/MainActivity.kt
+++ projects-2023/part2/01-Webtoon/app/src/main/java/com/hocheol/webtoon/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.hocheol.webtoon
 
 import android.os.Bundle
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.hocheol.webtoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            webView.webViewClient = WebViewClient()
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl("https://google.com")
+        }
     }
 }
