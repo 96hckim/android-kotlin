0817628d1dc5a4220e9b51b27aecbe2f155c82b2
96hckim <yp050431@gmail.com>
2023-06-04 13:10:27 +0900

    Button

--- projects-2023/part4/ComposeLab/app/src/main/java/com/hocheol/composelab/MainActivity.kt
+++ projects-2023/part4/ComposeLab/app/src/main/java/com/hocheol/composelab/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hocheol.composelab
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hocheol.composelab.ui.theme.ComposeLabTheme
 
@@ -23,9 +32,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                ButtonExample {
+                    Toast.makeText(this, "Send clicked.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -48,10 +56,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun ButtonExample(onButtonClicked: () -> Unit) {
+    Button(
+        onClick = onButtonClicked,
+        enabled = true,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = CircleShape,
+        contentPadding = PaddingValues(20.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Send,
+            contentDescription = "Send"
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Send")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeLabTheme {
-        Greeting("Android")
+        ButtonExample {
+
+        }
     }
 }
