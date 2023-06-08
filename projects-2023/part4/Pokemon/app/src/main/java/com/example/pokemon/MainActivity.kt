d1db38b5d5c08fda9962d9d42888b3be48664c06
hckim <yp050431@gmail.com>
2023-06-08 13:17:14 +0900

    포케몬 앱

--- projects-2023/part4/Pokemon/app/src/main/java/com/example/pokemon/MainActivity.kt
+++ projects-2023/part4/Pokemon/app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -6,20 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pokemon.screen.DetailScreen
+import com.example.pokemon.screen.MainScreen
 import com.example.pokemon.ui.theme.PokemonTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokemonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopLevel()
                 }
             }
         }
@@ -27,14 +37,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun TopLevel(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        "Home",
+        modifier = modifier
+    ) {
+        composable("Home") {
+            MainScreen(
+                onPokemonClick = {
+                    val pokemonId = it.substringAfter("pokemon/")
+                        .substringBefore("/")
+                        .toInt()
+                    navController.navigate("Detail/${pokemonId}")
+                }
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokemonTheme {
-        Greeting("Android")
+        composable(
+            "Detail/{pokemonId}",
+            arguments = listOf(
+                navArgument("pokemonId") {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            val pokemonId = it.arguments?.getInt("pokemonId") as Int
+            DetailScreen(
+                pokemonId = pokemonId,
+                onUpButtonClick = {
+                    navController.navigate("Home") {
+                        popUpTo("Home") {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
     }
 }
