d21717ead165b7b3111f5dd4dfa0ba512bc3a365
96hckim <yp050431@gmail.com>
2023-08-05 01:29:38 +0900

    First Commit

---
+++ projects-2023/part2/02-Recorder/app/src/main/java/com/hocheol/recorder/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hocheol.recorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hocheol.recorder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
