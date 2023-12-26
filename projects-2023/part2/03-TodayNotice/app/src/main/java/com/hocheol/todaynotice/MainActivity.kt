94561238bb558f24e2b0fb36384f7c99ee17a9b6
96hckim <yp050431@gmail.com>
2023-12-26 14:16:54 +0900

    Socket 을 활용하여 Client 앱 만들기

--- projects-2023/part2/03-TodayNotice/app/src/main/java/com/hocheol/todaynotice/MainActivity.kt
+++ projects-2023/part2/03-TodayNotice/app/src/main/java/com/hocheol/todaynotice/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.hocheol.todaynotice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread {
+            try {
+                val socket = Socket("10.0.2.2", 8080)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 127.0.0.1:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String?
+                while (reader.readLine().also { input = it } != null) {
+                    println("Received response: $input")
+                }
+
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e: Exception) {
+                Log.e("Client", e.toString())
+            }
+        }.start()
     }
 }
