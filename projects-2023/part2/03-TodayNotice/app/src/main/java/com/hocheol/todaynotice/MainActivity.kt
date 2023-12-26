cdf5d6774074cd78184a0787558973ae4af8df04
96hckim <yp050431@gmail.com>
2023-12-26 14:56:32 +0900

    클라이언트 앱 UI 그리기

--- projects-2023/part2/03-TodayNotice/app/src/main/java/com/hocheol/todaynotice/MainActivity.kt
+++ projects-2023/part2/03-TodayNotice/app/src/main/java/com/hocheol/todaynotice/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.hocheol.todaynotice
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import com.hocheol.todaynotice.databinding.ActivityMainBinding
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -12,33 +15,52 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val client = OkHttpClient()
+        var serverHost = ""
 
-        val request: Request = Request.Builder()
-            .url("http://10.0.2.2:8080")
-            .build()
-
-        val callback = object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e(TAG, e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val body = response.body?.string()
-                    Log.e(TAG, "$body")
-                }
-            }
+        binding.serverHostEditText.addTextChangedListener {
+            serverHost = it.toString()
         }
 
-        client.newCall(request).enqueue(callback)
-    }
+        binding.confirmButton.setOnClickListener {
+            val request: Request = Request.Builder()
+                .url("http://$serverHost:8080")
+                .build()
 
-    companion object {
-        private const val TAG = "Client"
+            val callback = object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "수신에 실패했습니다. $e", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val body = response.body?.string()
+
+                        runOnUiThread {
+                            binding.noticeTextView.isVisible = true
+                            binding.noticeTextView.text = body
+
+                            binding.serverHostEditText.isVisible = false
+                            binding.confirmButton.isVisible = false
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+            client.newCall(request).enqueue(callback)
+        }
     }
 }
