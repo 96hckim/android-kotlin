f38ecea228ff47a179666e78bfa5103687f47841
96hckim <yp050431@gmail.com>
2024-01-26 12:13:04 +0900

    Retrofit 을 이용해 서버 데이터 블러오기 (1)

--- projects-2023/part2/04-GitHubRepoExplorer/app/src/main/java/com/hocheol/githubrepoexplorer/MainActivity.kt
+++ projects-2023/part2/04-GitHubRepoExplorer/app/src/main/java/com/hocheol/githubrepoexplorer/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.hocheol.githubrepoexplorer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.hocheol.githubrepoexplorer.databinding.ActivityMainBinding
+import com.hocheol.githubrepoexplorer.model.Repo
+import com.hocheol.githubrepoexplorer.network.GitHubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +20,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val gitHubService = retrofit.create(GitHubService::class.java)
+        gitHubService.listRepos("96hckim").enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                Log.e(TAG, "onResponse: ${response.body()?.toString()}")
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+            }
+        })
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
---
+++ projects-2023/part2/04-GitHubRepoExplorer/app/src/main/java/com/hocheol/githubrepoexplorer/network/GitHubService.kt
@@ -0,0 +1,11 @@
+package com.hocheol.githubrepoexplorer.network
+
+import com.hocheol.githubrepoexplorer.model.Repo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+    @GET("users/{username}/repos")
+    fun listRepos(@Path("username") username: String): Call<List<Repo>>
+}
