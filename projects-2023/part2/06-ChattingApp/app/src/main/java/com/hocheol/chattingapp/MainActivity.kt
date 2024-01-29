04a1ee048e801801367b8f9217fbe55039004334
96hckim <yp050431@gmail.com>
2024-01-29 12:11:36 +0900

    사용자 목록 화면 그리기

--- projects-2023/part2/06-ChattingApp/app/src/main/java/com/hocheol/chattingapp/MainActivity.kt
+++ projects-2023/part2/06-ChattingApp/app/src/main/java/com/hocheol/chattingapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.hocheol.chattingapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.hocheol.chattingapp.databinding.ActivityMainBinding
+import com.hocheol.chattingapp.userlist.UserFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,5 +31,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            val fragment: Fragment? = when (it.itemId) {
+                R.id.userList -> {
+                    UserFragment()
+                }
+
+                R.id.chatRoomList -> {
+                    UserFragment()
+                }
+
+                R.id.myPage -> {
+                    UserFragment()
+                }
+
+                else -> null
+            }
+
+            if (fragment != null) {
+                replaceFragment(fragment)
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.frameLayout, fragment)
+                commit()
+            }
     }
 }
---
+++ projects-2023/part2/06-ChattingApp/app/src/main/java/com/hocheol/chattingapp/userlist/UserItem.kt
@@ -0,0 +1,7 @@
+package com.hocheol.chattingapp.userlist
+
+data class UserItem(
+    val userId: String,
+    val username: String,
+    val description: String
+)
