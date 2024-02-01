94d37b8e298db54cdd25968ed992830e3d7875ad
96hckim <yp050431@gmail.com>
2024-02-01 15:09:10 +0900

    지도에 맛집 목록 표시하기

--- projects-2023/part2/08-RestaurantMap/app/src/main/java/com/hocheol/restaurantmap/MainActivity.kt
+++ projects-2023/part2/08-RestaurantMap/app/src/main/java/com/hocheol/restaurantmap/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.hocheol.restaurantmap
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import com.hocheol.restaurantmap.databinding.ActivityMainBinding
-import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.Tm128
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,13 +29,46 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding.mapView.getMapAsync(this)
 
-        SearchRepository.getGoodRestaurants("서울").enqueue(object : Callback<SearchResult> {
-            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-                Log.d(TAG, "onResponse: ${response.body()}")
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return if (!query.isNullOrEmpty()) {
+                    SearchRepository.getGoodRestaurants(query).enqueue(object : Callback<SearchResult> {
+                        override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
+                            val searchItems = response.body()?.items.orEmpty()
+
+                            if (searchItems.isEmpty()) {
+                                Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+                                return
+                            } else if (::naverMap.isInitialized.not()) {
+                                Toast.makeText(this@MainActivity, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                                return
+                            }
+
+                            val markers = searchItems.map {
+                                val position = Tm128(it.mapx.toDouble(), it.mapy.toDouble()).toLatLng()
+                                Marker(position).apply {
+                                    captionText = it.title
+                                    map = naverMap
+                                }
+                            }
+
+                            val cameraUpdate = CameraUpdate.scrollTo(markers.first().position)
+                                .animate(CameraAnimation.Easing)
+                            naverMap.moveCamera(cameraUpdate)
+                        }
+
+                        override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                            t.printStackTrace()
+                        }
+                    })
+                    false
+                } else {
+                    true
+                }
             }
 
-            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-                t.printStackTrace()
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
             }
         })
     }
@@ -75,10 +110,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(mapObject: NaverMap) {
         naverMap = mapObject
-
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
-            .animate(CameraAnimation.Easing)
-        naverMap.moveCamera(cameraUpdate)
     }
 
     companion object {
