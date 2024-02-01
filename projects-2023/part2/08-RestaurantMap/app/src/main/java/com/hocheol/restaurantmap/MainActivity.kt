1f8b6b59ab6122c0fad01e8b93a1400fef27cf30
96hckim <yp050431@gmail.com>
2024-02-01 13:49:09 +0900

    네이버 지도 사용 사용해보기

--- projects-2023/part2/08-RestaurantMap/app/src/main/java/com/hocheol/restaurantmap/MainActivity.kt
+++ projects-2023/part2/08-RestaurantMap/app/src/main/java/com/hocheol/restaurantmap/MainActivity.kt
@@ -3,14 +3,67 @@ package com.hocheol.restaurantmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hocheol.restaurantmap.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
+            .animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
     }
 }
