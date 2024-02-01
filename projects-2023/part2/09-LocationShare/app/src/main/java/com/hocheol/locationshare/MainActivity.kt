191dd2510ad6586b4fea8a4368350ef14622f0f4
96hckim <yp050431@gmail.com>
2024-02-01 18:00:39 +0900

    카카오톡 로그인 구현하기 (1)

--- projects-2023/part2/09-LocationShare/app/src/main/java/com/hocheol/locationshare/MainActivity.kt
+++ projects-2023/part2/09-LocationShare/app/src/main/java/com/hocheol/locationshare/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hocheol.locationshare
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.hocheol.locationshare.databinding.ActivityMainBinding
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var keyHash = Utility.getKeyHash(this)
+        Log.d(TAG, "keyHash: $keyHash")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
