02106bad22eca0b7960f2a989e722112a8a8f32d
96hckim <yp050431@gmail.com>
2024-02-06 14:17:00 +0900

    메인 탭 구조 구성하기

---
+++ projects-2023/part2/11-Starbucks/app/src/main/java/com/hocheol/starbucks/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.hocheol.starbucks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.hocheol.starbucks.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+    }
+}
--- projects-2023/part2/11-Starbucks/app/src/main/java/com/hocheol/starbucks/MainActivity.kt
+++ projects-2023/part2/11-Starbucks/app/src/main/java/com/hocheol/starbucks/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hocheol.starbucks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hocheol.starbucks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
