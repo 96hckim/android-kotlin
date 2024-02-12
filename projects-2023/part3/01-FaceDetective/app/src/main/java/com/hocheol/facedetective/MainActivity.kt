f57ee8d174b3ed7be5cf6edbf149674ccdb140c8
96hckim <yp050431@gmail.com>
2024-02-12 09:31:14 +0900

    Main UI 그리기

--- projects-2023/part3/01-FaceDetective/app/src/main/java/com/hocheol/facedetective/MainActivity.kt
+++ projects-2023/part3/01-FaceDetective/app/src/main/java/com/hocheol/facedetective/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hocheol.facedetective
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.hocheol.facedetective.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startDetectButton.setOnClickListener {
+            it.isVisible = false
+        }
     }
 }
