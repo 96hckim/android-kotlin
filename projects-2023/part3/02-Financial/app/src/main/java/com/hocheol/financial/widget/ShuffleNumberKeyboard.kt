9894bbbc563778378a55b51ab41eea76a3f5ea54
96hckim <yp050431@gmail.com>
2024-02-12 14:18:52 +0900

    보안 키패드 - Shuffle 키패드 만들기 2

--- projects-2023/part3/02-Financial/app/src/main/java/com/hocheol/financial/widget/ShuffleNumberKeyboard.kt
+++ projects-2023/part3/02-Financial/app/src/main/java/com/hocheol/financial/widget/ShuffleNumberKeyboard.kt
@@ -3,6 +3,7 @@ package com.hocheol.financial.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.view.children
@@ -13,13 +14,17 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : GridLayout(context, attrs, defStyleAttr) {
+) : GridLayout(context, attrs, defStyleAttr), View.OnClickListener {
 
     private var _binding: WidgetShuffleNumberKeyboardBinding? = null
     private val binding get() = _binding!!
 
+    private var listener: KeyPadListener? = null
+
     init {
         _binding = WidgetShuffleNumberKeyboardBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.view = this
+        binding.clickListener = this
         shuffle()
     }
 
@@ -37,6 +42,31 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
                 view.text = keyNumbers[randomIndex].toString()
                 keyNumbers.removeAt(randomIndex)
             }
+        }
+    }
+
+    fun setKeyPadListener(keyPadListener: KeyPadListener) {
+        this.listener = keyPadListener
+    }
+
+    fun onClickDelete() {
+        listener?.onClickDelete()
+    }
+
+    fun onClickDone() {
+        listener?.onClickDone()
+    }
+
+    interface KeyPadListener {
+        fun onClickNumber(num: String)
+        fun onClickDelete()
+        fun onClickDone()
+    }
+
+    override fun onClick(v: View?) {
+        v ?: return
+        if (v is TextView && v.tag != null) {
+            listener?.onClickNumber(v.text.toString())
         }
     }
 }
