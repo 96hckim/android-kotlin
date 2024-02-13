c4745ea12bcd0e2532e1fbd65d8b349753bc8016
96hckim <yp050431@gmail.com>
2024-02-13 10:10:03 +0900

    휴대폰 본인 인증 UI 구성

---
+++ projects-2023/part3/02-Financial/app/src/main/java/com/hocheol/financial/util/ViewUtil.kt
@@ -0,0 +1,37 @@
+package com.hocheol.financial.util
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+object ViewUtil {
+
+    fun EditText.setOnEditorActionListener(action: Int, invoke: () -> Unit) {
+        setOnEditorActionListener { v, actionId, event ->
+            if (actionId == action) {
+                invoke()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    fun EditText.showKeyboard() {
+        this.requestFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    fun EditText.showKeyboardDelay() {
+        postDelayed({
+            showKeyboard()
+        }, 200L)
+    }
+
+    fun EditText.hideKeyboard() {
+        this.clearFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+    }
+}
