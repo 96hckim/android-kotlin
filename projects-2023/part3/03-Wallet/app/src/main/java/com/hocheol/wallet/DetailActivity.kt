831a4705d34f1e69ffa79ae6cc924c705c10ad6b
96hckim <yp050431@gmail.com>
2024-02-13 15:17:42 +0900

    상세 화면 데이터 연결하기

--- projects-2023/part3/03-Wallet/app/src/main/java/com/hocheol/wallet/DetailActivity.kt
+++ projects-2023/part3/03-Wallet/app/src/main/java/com/hocheol/wallet/DetailActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import com.hocheol.wallet.databinding.ActivityDetailBinding
+import com.hocheol.wallet.model.DetailItem
+import com.hocheol.wallet.model.Type
+import java.util.Calendar
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,12 +22,124 @@ class DetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initView()
+
+        adapter.submitList(mockData())
     }
 
     private fun initView() {
         binding.cardTitleTextView.text = intent.getStringExtra(CARD_NAME)
         binding.cardLayout.backgroundTintList = intent.getParcelableExtra(CARD_COLOR) as? ColorStateList
         binding.historyRecyclerView.adapter = adapter
+    }
+
+    private fun mockData(): List<DetailItem> {
+        fun createDate(year: Int, month: Int, day: Int) = Calendar.getInstance().apply {
+            set(year, month, day)
+        }.time
+
+        val list = mutableListOf<DetailItem>().apply {
+            add(
+                DetailItem(
+                    1,
+                    createDate(2023, 1, 6),
+                    "A상점",
+                    24000,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    2,
+                    createDate(2023, 1, 6),
+                    "B상점",
+                    120210,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    3,
+                    createDate(2023, 1, 1),
+                    "온라인 마트",
+                    11000,
+                    Type.CANCEL
+                )
+            )
+            add(
+                DetailItem(
+                    4,
+                    createDate(2023, 1, 1),
+                    "온라인 마트",
+                    11000,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    5,
+                    createDate(2022, 12, 11),
+                    "대형 마트",
+                    11000,
+                    Type.POINT
+                )
+            )
+
+            add(
+                DetailItem(
+                    6,
+                    createDate(2022, 12, 11),
+                    "대형 마트",
+                    1100000,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    7,
+                    createDate(2022, 11, 10),
+                    "창고형 마트",
+                    230000,
+                    Type.CANCEL
+                )
+            )
+
+            add(
+                DetailItem(
+                    8,
+                    createDate(2022, 11, 8),
+                    "창고형 마트",
+                    230000,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    9,
+                    createDate(2022, 11, 1),
+                    "주유소",
+                    50000,
+                    Type.PAY
+                )
+            )
+
+            add(
+                DetailItem(
+                    10,
+                    createDate(2022, 10, 15),
+                    "음식점",
+                    56000,
+                    Type.PAY
+                )
+            )
+        }
+
+
+        return list
     }
 
     companion object {
