afcffb223e20a61f8ceec7740d75c3ec6f021a6d
96hckim <yp050431@gmail.com>
2024-02-16 11:07:30 +0900

    Api 호출하기

--- projects-2023/part3/06-ShoppingMall/app/build.gradle.kts
+++ projects-2023/part3/06-ShoppingMall/app/build.gradle.kts
@@ -54,6 +54,14 @@ dependencies {
     // Coil
     implementation("io.coil-kt:coil:2.5.0")
 
+    // OkHttp && Retrofit
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
