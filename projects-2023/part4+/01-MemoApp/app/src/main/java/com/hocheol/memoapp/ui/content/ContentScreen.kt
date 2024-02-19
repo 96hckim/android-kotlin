d111586e9519588ca5baae54d226d14184cb79d0
96hckim <yp050431@gmail.com>
2024-02-19 16:40:13 +0900

    "상태 읽기 연기" 최적화 後

--- projects-2023/part4+/01-MemoApp/app/src/main/java/com/hocheol/memoapp/ui/content/ContentScreen.kt
+++ projects-2023/part4+/01-MemoApp/app/src/main/java/com/hocheol/memoapp/ui/content/ContentScreen.kt
@@ -38,7 +38,9 @@ fun ContentScreen(memoId: Int) {
     Box(Modifier.fillMaxSize()) {
         val scroll = rememberScrollState(0)
         Body(scroll)
-        Title(memo.text, scroll.value)
+        Title(memo.text) {
+            scroll.value
+        }
     }
 }
 
@@ -76,7 +78,7 @@ private fun Body(
 }
 
 @Composable
-private fun Title(memoText: String, scroll: Int) {
+private fun Title(memoText: String, scrollProvider: () -> Int) {
     val maxOffset = with(LocalDensity.current) { MaxTitleOffset.toPx() }
     val minOffset = with(LocalDensity.current) { MinTitleOffset.toPx() }
 
@@ -84,7 +86,7 @@ private fun Title(memoText: String, scroll: Int) {
         modifier = Modifier
             .heightIn(min = MaxTitleOffset)
             .offset {
-                val offset = (maxOffset - scroll).coerceAtLeast(minOffset)
+                val offset = (maxOffset - scrollProvider()).coerceAtLeast(minOffset)
                 IntOffset(x = 0, y = offset.toInt())
             }
             .fillMaxWidth()
