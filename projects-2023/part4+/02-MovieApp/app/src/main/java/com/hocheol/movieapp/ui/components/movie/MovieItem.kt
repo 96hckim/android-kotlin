a9df0393de192f4296ef48c22ecd069d0d36b784
96hckim <yp050431@gmail.com>
2024-02-20 07:55:28 +0900

    영화 카테고리 만들기

--- projects-2023/part4+/02-MovieApp/app/src/main/java/com/hocheol/movieapp/ui/components/movie/MovieItem.kt
+++ projects-2023/part4+/02-MovieApp/app/src/main/java/com/hocheol/movieapp/ui/components/movie/MovieItem.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hocheol.movieapp.R
 
 private val CARD_WIDTH = 150.dp
 
@@ -47,6 +52,17 @@ fun MovieItem() {
             ),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(12.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.baseline_star_rate_24),
+                tint = Color.Black.copy(
+                    alpha = 0.5f
+                ),
+                contentDescription = "rating icon"
+            )
+
             Text(
                 text = "5.0"
             )
