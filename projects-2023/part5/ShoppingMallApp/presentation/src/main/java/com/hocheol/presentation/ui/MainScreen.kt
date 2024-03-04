5cc2f47b705ec57712dd6993dffea19cc8d7d3b6
96hckim <yp050431@gmail.com>
2024-03-04 16:35:18 +0900

    홈화면 상단 검색바 구성하기

--- projects-2023/part5/ShoppingMallApp/presentation/src/main/java/com/hocheol/presentation/ui/MainScreen.kt
+++ projects-2023/part5/ShoppingMallApp/presentation/src/main/java/com/hocheol/presentation/ui/MainScreen.kt
@@ -3,31 +3,39 @@ package com.hocheol.presentation.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.hocheol.presentation.ui.theme.ShoppingMallAppTheme
+import com.hocheol.presentation.viewmodel.MainViewModel
 
-sealed class MainNavigationItem(var route: String, var name: String) {
-    data object Main : MainNavigationItem("Main", "Main")
-    data object Category : MainNavigationItem("Category", "Category")
-    data object MyPage : MainNavigationItem("MyPage", "MyPage")
+sealed class MainNavigationItem(var route: String, val icon: ImageVector, var name: String) {
+    data object Main : MainNavigationItem("Main", Icons.Filled.Home, "Main")
+    data object Category : MainNavigationItem("Category", Icons.Filled.Star, "Category")
+    data object MyPage : MainNavigationItem("MyPage", Icons.Filled.AccountBox, "MyPage")
 }
 
 @Preview(showBackground = true)
@@ -40,10 +48,14 @@ fun GreetingPreview() {
 
 @Composable
 fun MainScreen() {
+    val viewModel = hiltViewModel<MainViewModel>()
     val snackbarHostState = remember { SnackbarHostState() }
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+            MainHeader(viewModel)
+        },
         bottomBar = {
             MainBottomNavigationBar(navController = navController)
         },
@@ -58,6 +70,26 @@ fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainHeader(viewModel: MainViewModel) {
+    TopAppBar(
+        title = { Text("My App") },
+        actions = {
+            IconButton(
+                onClick = {
+                    viewModel.openSearchForm()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "SearchIcon"
+                )
+            }
+        }
+    )
+}
+
 
 @Composable
 fun MainBottomNavigationBar(navController: NavHostController) {
@@ -67,10 +99,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
         MainNavigationItem.MyPage
     )
 
-    NavigationBar(
-        containerColor = Color(0xffff0000),
-        contentColor = Color(0xff00ff00)
-    ) {
+    NavigationBar {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
@@ -90,7 +119,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
                 },
                 icon = {
                     Icon(
-                        imageVector = Icons.Filled.Home,
+                        imageVector = item.icon,
                         contentDescription = item.route
                     )
                 }
