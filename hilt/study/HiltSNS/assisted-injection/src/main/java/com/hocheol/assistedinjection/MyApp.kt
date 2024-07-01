4484172ad6ce2b75c3604da90aa72391482bdb83
96hckim <yp050431@gmail.com>
2024-07-01 10:13:48 +0900

    Custom Inject

--- hilt/study/HiltSNS/assisted-injection/src/main/java/com/hocheol/assistedinjection/MyApp.kt
+++ hilt/study/HiltSNS/assisted-injection/src/main/java/com/hocheol/assistedinjection/MyApp.kt
@@ -1,7 +1,28 @@
 package com.hocheol.assistedinjection
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.migration.CustomInject
+import dagger.hilt.android.migration.CustomInjection
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApp : Application()
+@CustomInject
+class MyApp : Application() {
+
+    @Inject
+    lateinit var userService: UserService
+
+    override fun onCreate() {
+        Log.d(TAG, "userService1: ${::userService.isInitialized}") // not initialized
+        super.onCreate()
+        Log.d(TAG, "userService2: ${::userService.isInitialized}") // not initialized
+        CustomInjection.inject(this)
+        Log.d(TAG, "userService3: ${::userService.isInitialized}") // initialized
+    }
+
+    companion object {
+        private const val TAG = "MyApp"
+    }
+}
